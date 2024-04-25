3c03acfa7a857e907cabc4ae9131a31ef5ea687a
Rachel Katz <rachel.katz@flatironschool.com>
2024-04-25 14:41:18 -0400

    demo done

--- theater_demo/server/app.py
+++ theater_demo/server/app.py
@@ -27,31 +27,72 @@ def index():
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ✅ 1. Refactor `/productions` to include a `GET` and `POST`
-@app.route('/productions')
+@app.route('/productions', methods=["GET","POST"])
 def all_productions():
-    q = Production.query.all()
-    prod_list = [p.to_dict() for p in q]
-    res = make_response(jsonify(prod_list), 200)
-    return res 
+    if(request.method == "GET"):
+        q = Production.query.all()
+        prod_list = [p.to_dict() for p in q]
+        res = make_response(jsonify(prod_list), 200)
+        return res 
+    elif(request.method == "POST"):
+        #import ipdb; ipdb.set_trace()
+        data = request.json 
+        new_prod = Production(
+            # use get so that if 'title' does not exist, we don't get an error and break our program
+            title=data.get('title'),
+            genre=data.get('genre'),
+            director=data.get('director'),
+            composer=data.get('composer'),
+            length=data.get('length'),
+            image=data.get('image'),
+            description=data.get('description'),
+            language=data.get('language'),
+            year=data.get('year')
+        )
 
-    # ✅ 2. Create a route to /productions for a POST request
-    # ✅ 2a. Get information from request.get_json() 
-    # ✅ 2b. Create new object
-    # ✅ 2c. Add and commit to db 
-    # ✅ 2d. Convert to dictionary / # 5c. use .to_dict
-    # ✅ 2e. Return as JSON
-    # ✅ 2f. Test in postman
+        db.session.add(new_prod)
+        db.session.commit()
+
+        return make_response(new_prod.to_dict(), 201)
+        
+        # ✅ 2. Create a route to /productions for a POST request
+        # ✅ 2a. Get information from request.get_json() 
+        # ✅ 2b. Create new object
+        # ✅ 2c. Add and commit to db 
+        # ✅ 2d. Convert to dictionary / # 5c. use .to_dict
+        # ✅ 2e. Return as JSON
+        # ✅ 2f. Test in postman
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ✅ 3. Create a delete request 
 # ✅ 3a. Refactor `/productions/:id` to take both a `GET` and a `DELETE`
-@app.route('/productions/<int:id>')
-def One_Production(id):
-    # ✅ 3b. Query for the wanted production
+@app.route('/productions/<int:id>', methods=["GET", "PATCH", "DELETE"])
+def production_by_id(id):
     q = Production.query.filter_by(id=id).first()
-    prod_dict = q.to_dict()
-    res = make_response(jsonify(prod_dict), 200)
-    return res
+
+    if not q:
+        return make_response({'not found'}, 404)
+    # ✅ 3b. Query for the wanted production
+
+    if(request.method=="GET"):
+        prod_dict = q.to_dict()
+        res = make_response(jsonify(prod_dict), 200)
+        return res
+    elif(request.method=="DELETE"):
+        db.session.delete(q)
+        db.session.commit()
+        return make_response({}, 204)
+    elif(request.method=="PATCH"):
+        data = request.json 
+        for attr in data:
+            setattr(q, attr, data.get(attr))
+        db.session.add(q)
+        db.session.commit()
+
+        return make_response(q.to_dict(), 200)
+
+        
+    
     # ✅ 3c. Use `session.delete`
     # ✅ 4. PATCH request
 
